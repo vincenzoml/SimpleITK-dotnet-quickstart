2e14c35055aa292cbeee3d87e82d261eca3fd214
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-04-29 18:52:27 +0200

    created csharp example

---
+++ csharp/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("SimpleITK Version: {0}\nITK Version: {1}",itk.simple.Version.VersionString(),itk.simple.Version.ITKVersionString());
+
